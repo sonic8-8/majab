3d89b75275a5e7880522e9bcfed670142665f72e
이승환 <seaung13@gmail.com>
2025-03-29 22:27:22 +0900

    chore: initial commit

---
+++ src/main/java/kr/kro/majab/MajabApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.majab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/kro/majab/MajabApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kro.majab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
