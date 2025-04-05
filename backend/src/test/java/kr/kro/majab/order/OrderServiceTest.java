4b85b36084e2a219654ff86167486ce482b3af8c
이승환 <seaung13@gmail.com>
2025-04-05 13:29:37 +0900

    test: 주문 생성 예외 테스트 추가 (존재하지 않는 상품, 가게, 사용자)
    test: 주문 생성 예외 테스트 추가 (트랜잭션 롤백)

--- backend/src/test/java/kr/kro/majab/order/OrderServiceTest.java
+++ backend/src/test/java/kr/kro/majab/order/OrderServiceTest.java
@@ -9,6 +9,7 @@ import kr.kro.majab.store.StoreRepository;
 import kr.kro.majab.store.StoreStatus;
 import kr.kro.majab.user.User;
 import kr.kro.majab.user.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,12 +18,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
+@Slf4j
 class OrderServiceTest {
 
     @Autowired
@@ -103,7 +110,7 @@ class OrderServiceTest {
 
     @DisplayName("재고 수량이 부족한 제품을 주문할 경우 예외가 발생한다")
     @Test
-    void createOrderStockException() {
+    void createOrder_StockException() {
         // given
         LocalDateTime registeredDateTime = LocalDateTime.now();
 
@@ -186,6 +193,115 @@ class OrderServiceTest {
         assertThatThrownBy(() -> orderService.createOrder(request, registeredDateTime))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("상품 재고가 없습니다");
+    }
+
+    @DisplayName("존재하지 않는 사용자가 주문할 경우 예외가 발생한다")
+    @Test
+    void createOrderException_UserNotExist() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Item item = createItem(10, 3000);
+        itemRepository.save(item);
+
+        Store store = createStore("가게", StoreStatus.OPEN);
+        storeRepository.save(store);
+
+        CreateOrderRequest request = CreateOrderRequest.builder()
+                .storeStatus(store.getStoreStatus())
+                .itemId(item.getId())
+                .storeId(store.getId())
+                .quantity(5)
+                .userId(Long.MAX_VALUE)
+                .build();
+
+        // when then
+        assertThatThrownBy(() -> orderService.createOrder(request, registeredDateTime))
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessage("해당 사용자가 존재하지 않습니다");
+    }
+
+    @DisplayName("존재하지 않는 가게의 상품을 주문할 경우 예외가 발생한다")
+    @Test
+    void createOrderException_StoreNotExist() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Item item = createItem(10, 3000);
+        itemRepository.save(item);
+
+        User user = createUser("사용자");
+        userRepository.save(user);
+
+        CreateOrderRequest request = CreateOrderRequest.builder()
+                .storeStatus(StoreStatus.OPEN)
+                .itemId(item.getId())
+                .storeId(Long.MAX_VALUE)
+                .quantity(5)
+                .userId(user.getId())
+                .build();
+
+        // when then
+        assertThatThrownBy(() -> orderService.createOrder(request, registeredDateTime))
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessage("해당 가게가 존재하지 않습니다");
+    }
+
+    @DisplayName("존재하지 않는 상품을 주문할 경우 예외가 발생한다")
+    @Test
+    void createOrderException_ItemNotExist() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Store store = createStore("가게", StoreStatus.OPEN);
+        storeRepository.save(store);
+
+        User user = createUser("사용자");
+        userRepository.save(user);
+
+        CreateOrderRequest request = CreateOrderRequest.builder()
+                .storeStatus(store.getStoreStatus())
+                .itemId(Long.MAX_VALUE)
+                .storeId(store.getId())
+                .quantity(5)
+                .userId(user.getId())
+                .build();
+
+        // when then
+        assertThatThrownBy(() -> orderService.createOrder(request, registeredDateTime))
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessage("해당 상품이 존재하지 않습니다");
+    }
+
+    @DisplayName("재고 수량 부족으로 예외 발생시 주문이 저장되지 않는다")
+    @Test
+    void createOrder_rollback() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Item item = createItem(10, 3000);
+        itemRepository.save(item);
+
+        Store store = createStore("가게", StoreStatus.OPEN);
+        storeRepository.save(store);
+
+        User user = createUser("사용자");
+        userRepository.save(user);
+
+        CreateOrderRequest request = CreateOrderRequest.builder()
+                .userId(user.getId())
+                .storeId(store.getId())
+                .itemId(item.getId())
+                .quantity(11)
+                .storeStatus(store.getStoreStatus())
+                .build();
+
+        // when then
+        assertThatThrownBy(() -> orderService.createOrder(request, registeredDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("상품 재고가 부족합니다");
+
+        assertThat(orderRepository.findAll()).isEmpty();
     }
 
 }
